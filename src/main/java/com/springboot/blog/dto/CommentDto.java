fa1868e28cd47e1f4ca78b91ca1d5593a56d53d7
Srivatsa Nayak <nayaksrivatsa15@gmail.com>
2024-11-23 20:12:04 +0530

    modify the exception message

--- src/main/java/com/springboot/blog/dto/CommentDto.java
+++ src/main/java/com/springboot/blog/dto/CommentDto.java
@@ -1,5 +1,8 @@
 package com.springboot.blog.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,15 @@ import lombok.NoArgsConstructor;
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "name should not be empty")
     private String name;
+
+    @NotEmpty(message = "email should not be empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "body should not be empty")
+    @Size(min = 10, message = "Comment Body should have at least 10 characters")
     private String body;
 }
--- src/main/java/com/springboot/blog/dto/PostDto.java
+++ src/main/java/com/springboot/blog/dto/PostDto.java
@@ -1,13 +1,25 @@
 package com.springboot.blog.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
+
+    @NotEmpty(message = "title should not be empty")
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    @NotEmpty(message = "message should not be empty")
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty(message = "content should not be empty")
+    @Size(min = 15, message = "Post content should have at least 15 characters")
     private String content;
+
     private Set<CommentDto> comments;
 }

6c00497d08c3b83201bfe5a618b47c1a242ce720
Srivatsa Nayak <nayaksrivatsa15@gmail.com>
2024-11-23 11:58:39 +0530

    add exception handler class

--- src/main/java/com/springboot/blog/service/PostService.java
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -10,7 +10,7 @@ public interface PostService {
 
     PostResponse getAllPosts(int pageNo , int pageSize, String sortBy, String sortDir);
 
-    PostDto getPostById(Long id);
+    PostDto getPostById(long id);
 
     PostDto updatePost(PostDto postDto , long id);
 

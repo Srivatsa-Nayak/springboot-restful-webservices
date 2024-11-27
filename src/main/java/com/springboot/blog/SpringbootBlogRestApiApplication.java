ec6966e85e7f57c992936a63b474ab0f7abe7a89
Srivatsa Nayak <nayaksrivatsa15@gmail.com>
2024-11-27 11:32:04 +0530

    implement swagger ui for documenting rest api

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,5 +1,9 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +11,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot BLog App Rest APIs",
+				description = "Spring Boot BLog App Rest APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Srivatsa",
+						email = "nayaksrivatsa15@gmail.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/Srivatsa-Nayak/spring_posts"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
